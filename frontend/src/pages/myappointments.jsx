6c18c629e2ab87d98263b8ab7dede257ef6aeb44
Yaman Abulaban <yamanabulaban@gmail.com>
2025-02-13 20:40:48 +0300

    Finished the style of MyAppointments page

--- frontend/src/pages/myappointments.jsx
+++ frontend/src/pages/myappointments.jsx
@@ -7,27 +7,27 @@ const MyAppointments = () => {
   const { doctors } = useContext(AppContext)
 
   return (
-    <div>
-      <p>My Appointments</p>
+    <div >
+      <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My Appointments</p>
       <div>
         {
-          doctors.slice(0,2).map((item, index)=>(
-            <div key={index}>
+          doctors.slice(0,3).map((item, index)=>(
+            <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
               <div>
-                <img src={item.image} alt="" />
+                <img className='w-32 bg-green-50' src={item.image} alt="" />
               </div>
-              <div>
-                <p>{item.name}</p>
+              <div className='flex-1 text-sm text-zinc-600'>
+                <p className='text-neutral-800 font-semibold'>{item.name}</p>
                 <p>{item.speciality}</p>
-                <p>Address:</p>
-                <p>{item.address.line1}</p>
-                <p>{item.address.line2}</p>
-                <p><span>Date & Time:</span> 25, July, 2025 | 8:30</p>
+                <p className='text-zinc-700 font-medium mt-1'>Address:</p>
+                <p className='text-xs'>{item.address.line1}</p>
+                <p className='text-xs'>{item.address.line2}</p>
+                <p className='text-sm mt-1'><span className='text-sm text-neural-700 font-medium'>Date & Time:</span> 25, July, 2025 | 8:30</p>
               </div>
               <div></div>
-              <div>
-                <button>Pay Now</button>
-                <button>Cancel Appointment</button>
+              <div className='flex flex-col gap-2 justify-end'>
+                <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Now</button>
+                <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>
               </div>
             </div>
           ))
