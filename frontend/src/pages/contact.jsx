c7ab358c0c1d3da460cc1a74888383833da16170
Yaman Abulaban <yamanabulaban@gmail.com>
2025-02-13 19:20:40 +0300

    Finished contact page

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -11,9 +11,16 @@ const Contact = () => {
           <p>CONTACT <span className='text-gray-700 font-semibold'>US</span></p>
         </div>
 
-        <div>
-          <img src={assets.contact_image} alt="" />
-          <div></div>
+        <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+          <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+          <div className='flex flex-col justify-center items-start gap-6'>
+            <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+            <p className='text-gray-500'>00000 7th Circle <br /> Amman, Jordan</p>
+            <p className='text-gray-500'>Tel: (000) 000-0000 <br />Email: kifaya@abulabandentalclinic.com</p>
+            <p className='font-semibold text-lg text-gray-600'>CAREERS AT Abulaban Dental Clinic</p>
+            <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+            <button className='border border-black px-8 py-4 text-sm hover:bg-primary hover:text-white transition-all duration-400'>Explore Jobs</button>
+          </div>
         </div>
     </div>
   )
